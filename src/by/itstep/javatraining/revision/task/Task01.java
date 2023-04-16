46501414c4e0c7101e3a8ae8adda9e46c14a4a7a
SergeyBelyaev <belyaevsergeyy@rambler.ru>
2023-04-16 14:38:20 +0300

    Task 01 well done

--- src/by/itstep/javatraining/revision/task/Task01.java
+++ src/by/itstep/javatraining/revision/task/Task01.java
@@ -36,6 +36,11 @@ package by.itstep.javatraining.revision.task;
 
 public class Task01 {
     public static int task01(int hour1, int minute1, int second1, int hour2, int minute2, int second2) {
+        if (hour1 >= 0 && hour1 < 24 && hour2 >= 0 && hour2 < 24 &&
+                minute1 >= 0 && minute1 < 60 && minute2 >= 0 && minute2 < 60 &&
+                second1 >= 0 && second1 < 60 && second2 >= 0 && second2 < 60) {
+            return Math.abs(((hour2 * 60 + minute2) * 60 + second2) - ((hour1 * 60 + minute1) * 60 + second1));
+        }
         return -1;
     }
 }
